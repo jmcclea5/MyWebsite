2bf848e37e6a11e714cf09846484a04738d14dc9
jmcclea5 <104521193+jmcclea5@users.noreply.github.com>
2022-05-05 12:25:47 -0400

    Update Website.js

--- Website.js
+++ Website.js
@@ -1,3 +1,13 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
